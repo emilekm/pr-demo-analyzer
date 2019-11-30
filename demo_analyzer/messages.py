410dde07f96c5812d6ad1534e0cec80b77491198
cassius23 <emilekm@gmail.com>
2019-11-30 17:40:38 +0100

    Add some messages

---
+++ demo_analyzer/messages.py
@@ -0,0 +1,41 @@
+from demo_analyzer import serializers
+
+
+class Header(serializers.Serializer):
+    length = serializers.UInt16Field()
+
+
+class Type(serializers.Serializer):
+    type = serializers.ByteField()
+
+
+class MapSerializer(serializers.Serializer):
+    name = serializers.StringField()
+    gamemode = serializers.StringField()
+    layer = serializers.UInt8Field()
+
+
+class ServerDetails(serializers.Serializer):
+    version = serializers.Int32Field()
+    demo_time_per_tick = serializers.FloatField()
+    ip_port = serializers.StringField()
+    server_name = serializers.StringField()
+    max_players = serializers.UInt8Field()
+    round_length = serializers.UInt16Field()
+    briefing_time = serializers.UInt16Field()
+    map = MapSerializer()
+    blufor_team = serializers.StringField()
+    opfor_team = serializers.StringField()
+    start_time = serializers.TimestampField(fmt='I')
+    tickets1 = serializers.UInt16Field()
+    tickets2 = serializers.UInt16Field()
+
+
+class Ticks(serializers.Serializer):
+    ticks = serializers.UInt8Field()
+
+
+class Kill(serializers.Serializer):
+    attacker = serializers.UInt8Field()
+    victim = serializers.UInt8Field()
+    weapon = serializers.StringField()
