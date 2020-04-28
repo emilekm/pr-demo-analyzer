b1df48609d98950e57175b445b1028a636670db9
cassius23 <emilekm@gmail.com>
2020-04-28 22:25:26 +0200

    Add more messages

--- demo_analyzer/messages.py
+++ demo_analyzer/messages.py
@@ -1,8 +1,5 @@
-from demo_analyzer import serializers
-
-
-class Type(serializers.Serializer):
-    type = serializers.ByteField()
+from demo_analyzer import constants, serializers
+from demo_analyzer.constants import FormatStrings, MessageTypes
 
 
 class MapSerializer(serializers.Serializer):
@@ -22,7 +19,7 @@ class ServerDetails(serializers.Serializer):
     map = MapSerializer()
     blufor_team = serializers.StringField()
     opfor_team = serializers.StringField()
-    start_time = serializers.TimestampField(fmt='I')
+    start_time = serializers.TimestampField(fmt=FormatStrings.UINT32)
     tickets1 = serializers.UInt16Field()
     tickets2 = serializers.UInt16Field()
 
@@ -37,12 +34,102 @@ class PlayerAdd(serializers.Serializer):
         many = True
 
 
+class PlayerVehicle(serializers.Serializer):
+    id = serializers.Int16Field()
+    seat_name = serializers.StringField()
+    seat_number = serializers.Int8Field()
+
+    def check_id(self, ret):
+        if ret['id'] < 0:
+            raise serializers.SkipField()
+
+    def validate_seat_name(self, raw_value, ret):
+        self.check_id(ret)
+
+    def validate_seat_number(self, raw_value, ret):
+        self.check_id(ret)
+
+
+
+class Position(serializers.Serializer):
+    x = serializers.Int16Field()
+    y = serializers.Int16Field()
+    z = serializers.Int16Field()
+
+
+class PlayerUpdate(serializers.Serializer):
+    flags = serializers.FlagsField(
+        fmt=FormatStrings.UINT16,
+        flags=constants.PlayerUpdateFlags,
+    )
+    id = serializers.UInt8Field()
+    team = serializers.Int8Field()
+    squad = serializers.UInt8Field()
+    vehicle = PlayerVehicle()
+    health = serializers.Int8Field()
+    score = serializers.Int16Field()
+    teamwork_score = serializers.Int16Field()
+    kills = serializers.Int16Field()
+    deaths = serializers.Int16Field()
+    ping = serializers.Int16Field()
+    is_alive = serializers.BoolField()
+    is_joining = serializers.BoolField()
+    position = Position()
+    rotation = serializers.Int16Field()
+    kit_name = serializers.StringField()
+
+    class Meta:
+        many = True
+
+
 class PlayerRemove(serializers.Serializer):
     id = serializers.UInt8Field()
 
 
-class Ticks(serializers.Serializer):
-    ticks = serializers.UInt8Field()
+class VehicleAdd(serializers.Serializer):
+    id = serializers.Int16Field()
+    name = serializers.StringField()
+    max_health = serializers.UInt16Field()
+
+    class Meta:
+        many = True
+
+
+class VehicleUpdate(serializers.Serializer):
+    flags = serializers.FlagsField(
+        constants.VehicleUpdateFlags,
+        fmt=FormatStrings.UINT8,
+    )
+    id = serializers.Int16Field()
+    team = serializers.Int8Field()
+    position = Position()
+    rotation = serializers.Int16Field()
+    health = serializers.Int8Field()
+
+    class Meta:
+        many = True
+
+
+class VehicleDestroyed(serializers.Serializer):
+    id = serializers.Int16Field()
+    is_killer_known = serializers.BoolField()
+    killer = serializers.UInt8Field()
+
+
+class FobAdd(serializers.Serializer):
+    id = serializers.Int32Field()
+    team = serializers.Int8Field()
+    position = Position()
+
+    class Meta:
+        many = True
+
+
+class FobRemove(serializers.Serializer):
+    id = serializers.Int32Field()
+
+    class Meta:
+        many = True
 
 
 class Kill(serializers.Serializer):
@@ -58,3 +145,7 @@ class Revive(serializers.Serializer):
 
 class TeamTickets(serializers.Serializer):
     tickets = serializers.UInt16Field()
+
+
+class Ticks(serializers.Serializer):
+    ticks = serializers.UInt8Field()
